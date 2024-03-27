5dcf2b9c8886425390e5739592d52a67fe1caa76
CaptAwesome4500 <elijah@theparents.net>
2024-03-27 14:26:29 -0400

    Update cache_buster.js

--- scripts/cache_buster.js
+++ scripts/cache_buster.js
@@ -1,5 +1,5 @@
 (function() {
-	const href = '../styles.css';
+	const href = 'https://captawesome4500.github.io/styles.css';
 	const cacheBuster = Math.round(new Date().getTime() / 1000);
 	const link = document.createElement('link');
 	link.setAttribute('href', `${href}?cb=${cacheBuster}`);
