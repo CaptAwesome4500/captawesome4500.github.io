342d7616daeb23d0b036b3d2f7bce8b535d6627a
CaptAwesome4500 <elijah@theparents.net>
2024-04-14 15:41:24 -0400

    Update load_lang.js

--- experimental/load_lang.js
+++ experimental/load_lang.js
@@ -40,3 +40,31 @@ document.getElementById('languageSelect').addEventListener('change', function ()
 updateOutput('en', {});
 
 export { translationsData }; // Export translations object
+
+
+// Now you can use translationsData.greeting in this file
+var text = 0;
+
+function next()
+{
+	//console.log(text)
+	text++;
+	if (text == 2){
+		text = 0
+	}
+
+	if (text == 0){
+		document.getElementById("text_go_here").innerHTML = translationsData.greeting;
+	}
+
+	else if (text == 1){
+		document.getElementById("text_go_here").innerHTML = translationsData.farewell;
+	}
+	console.log(text)
+}
+
+// Add event listener to the button
+document.getElementById('nextButton').addEventListener('click', function() {
+    // Call the next function when the button is clicked
+    next();
+});
