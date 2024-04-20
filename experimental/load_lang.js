79bfaac803fb2351700e202b6d83c9eef44a5d56
CaptAwesome4500 <elijah@theparents.net>
2024-04-20 16:51:25 -0400

    Attempted to Fix Lexion

--- experimental/load_lang.js
+++ experimental/load_lang.js
@@ -291,9 +291,11 @@ function next_text()
 
 function lexion_riddle()
 {
-	const anwser = prompt(translationsData.actI.levelI.lexion_anwser + ":");
+	const anwser = prompt(translationsData.actI.levelI.lexionAnwser + ":");
 	
-	if (anwser.includes("seven") == true || anwser.includes("7") == true){
+	anwser = anwser.toUpperCase();
+
+	if (anwser == translationsData.actI.levelI.anwser ||  anwser == "7"){
 		
 		level[2]++;
 		anwser_correct = true;
