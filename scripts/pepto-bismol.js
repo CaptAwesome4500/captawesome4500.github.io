fb39e1e372a1db4e3c0846f88afcb373aa89c6eb
CaptAwesome4500 <elijah@theparents.net>
2024-05-22 15:40:47 -0400

    Fixed ANOTHER error

--- scripts/pepto-bismol.js
+++ scripts/pepto-bismol.js
@@ -3,5 +3,5 @@ const date = new Date();
 const month = date.getMonth();
 const day = date.getDay();
 
-console.log("Month:" month);
-console.log("Day:" day);
+console.log("Month:" + month);
+console.log("Day:" + day);
