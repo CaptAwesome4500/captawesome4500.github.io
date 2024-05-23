1d8731bcd29d0980831fe23a3db84b8c001aaf31
CaptAwesome4500 <elijah@theparents.net>
2024-05-23 11:55:52 -0400

    Update pepto-bismol.js

--- scripts/pepto-bismol.js
+++ scripts/pepto-bismol.js
@@ -1,8 +1,10 @@
-// Create a new Date object for the current date and time
 const currentDate = new Date();
 
-// Get the day of the month (1-31)
 const day = currentDate.getDate();
+const month = currentDate.getMonth();
 
-// Output the value of day to the console (for testing purposes)
-console.log(day);
+//Dec. 12 & 13
+
+if (month == 5){
+  document.body.style.backgroundColor = 'red';
+}
